01b855e5b2b3eda4b537f01104c6169d323c8163
Guilherme Machado <gui.mp8@gmail.com>
2024-06-14 15:33:06 -0300

    feat: addong configuration file

--- src/main.py
+++ src/main.py
@@ -1,25 +1,33 @@
 import os
 
+from utils.config import load_config
 from datasources.the_guardian import TheGuardian
 from analytics.bigquery import BigQueryR
 
-def main():
+def main(config):
+    
+    print(config)
+    
     the_guardian = TheGuardian()
-    the_guardian.site_url = "https://www.theguardian.com/au"
-    data = the_guardian.get_structured_page_content()
+    
+    for table in config["extraction"]["site"]["the_guardian"]["table"]:
+        
+        the_guardian.site_url = table["site_url"]
+        data = the_guardian.get_structured_page_content()
 
-    bq = BigQueryR(
-            project_name="news-extraction-426201",
-            dataset_name="challenge",
-            table_name="news"
-        )
+        bq = BigQueryR(
+                project_name=config["project_id"],
+                dataset_name=config["extraction"]["site"]["the_guardian"]["dataset_name"],
+                table_name=table["name"]
+            )
 
-    bq.create_dataset()
-    bq.create_table()
-    bq.insert_rows(data=data)
+        bq.create_dataset()
+        bq.create_table()
+        bq.insert_rows(data=data)
 
 
 
 if __name__ == '__main__':
+    config = load_config()
     # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "./news-extraction.json"
-    main()
+    main(config)
